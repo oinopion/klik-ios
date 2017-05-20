7ea3a985e2389cdf5fecb58ef928bc692e148443
Tomek Paczkowski <tomek@hauru.eu>
2017-05-20 22:17:42 +0100

    Added paging between counters

---
+++ Klik/RootViewController.swift
@@ -0,0 +1,72 @@
+import UIKit
+
+class RootViewController: UIViewController, UIPageViewControllerDelegate {
+    var pageViewController: UIPageViewController!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupPageViewController()
+    }
+
+    func counterViewController() -> CounterViewController {
+        return storyboard!.instantiateViewController(
+            withIdentifier: "CounterViewController") as! CounterViewController
+
+    }
+
+    private func setupPageViewController() {
+        pageViewController = UIPageViewController(transitionStyle: .scroll,
+                                                  navigationOrientation: .horizontal,
+                                                  options: nil)
+        pageViewController.setViewControllers([counterViewController()],
+                                              direction: .forward,
+                                              animated: true,
+                                              completion: nil)
+        pageViewController.dataSource = self
+        for view in pageViewController.view.subviews {
+            if let scrollView = view as? UIScrollView {
+                scrollView.delaysContentTouches = false
+            }
+        }
+
+        self.addChildViewController(pageViewController)
+        self.disableTouchDelay(inChildrenOf: pageViewController.view)
+        view.addSubview(pageViewController.view)
+        pageViewController.didMove(toParentViewController: self)
+    }
+
+    private func disableTouchDelay(inChildrenOf view : UIView) {
+        for view in view.subviews {
+            if let scrollView = view as? UIScrollView {
+                scrollView.delaysContentTouches = false
+            }
+        }
+    }
+    
+}
+
+extension RootViewController : UIPageViewControllerDataSource {
+
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            viewControllerBefore viewController: UIViewController) -> UIViewController? {
+
+        return counterViewController()
+    }
+
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            viewControllerAfter viewController: UIViewController) -> UIViewController? {
+
+        return counterViewController()
+    }
+
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return 3
+    }
+
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        return 1
+    }
+}
+
+
+
